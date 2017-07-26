9fc078c4634b1846b6a7e0cf56f27eeafb7dd25b
Tyler <tyler.slater@joltup.com>
2017-07-26 16:39:43 -0600

    added nodejs shims

---
+++ global.js
@@ -0,0 +1,20 @@
+// Inject node globals into React Native global scope.
+global.Buffer = require('buffer').Buffer;
+global.process = require('process');
+global.process.env.NODE_ENV = __DEV__ ? 'development' : 'production';
+// global.process.env.DEBUG='join-monster'
+global.process.stderr = {};
+// Needed so that 'stream-http' chooses the right default protocol.
+global.location = {
+  protocol: 'file:',
+};
+
+// Don't do this in production. You're going to want to patch in
+// https://github.com/mvayngrib/react-native-randombytes or similar.
+global.crypto = {
+  getRandomValues(byteArray) {
+    for (let i = 0; i < byteArray.length; i++) {
+      byteArray[i] = Math.floor(256 * Math.random());
+    }
+  },
+};
